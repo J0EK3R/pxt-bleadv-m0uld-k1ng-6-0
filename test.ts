2c059779c555f66993c9787e664b4520b0eea17e
J0EK3R <J0EK3R@users.noreply.github.com>
2025-02-13 14:24:43 +0100

    Update pxt.json, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,9 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+input.onButtonPressed(Button.A, function () {
+    MK6.setChannelAndSend(MK6.Module.M1, MK6.Channel.A, 50)
+})
+input.onButtonPressed(Button.B, function () {
+    MK6.setChannelAndSend(MK6.Module.M1, MK6.Channel.A, 0)
+})
+MK6.init(MK6.Module.M1)
+MK6.setChannelOffset(MK6.Module.M1, MK6.Channel.A, 20)
